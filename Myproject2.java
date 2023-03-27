12b7d8c6fd9da747d2bc279b4adb282fecc987d2
Pearmai <pearmai.wann@kmutt.ac.th>
2023-03-27 14:02:11 +0700

    1 command

---
+++ Myproject2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package myproject2;
+
+/**
+ *
+ * @author bunnymoon
+ */
+public class Myproject2 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}
